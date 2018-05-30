e389436a5e4c601ade79ebc702cd6237f0b2e6b1
ters <blancofrancisco34@gmail.com>
2018-05-30 17:55:42 -0400

    Agregando metodo Listado de procesos (opcion 2)

--- Programa.c
+++ Programa.c
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
+void Listado_de_Procesos();
 int main(int argc, char const *argv[])
 {
 	int opcion=-1;
@@ -29,4 +29,7 @@ int main(int argc, char const *argv[])
 
 
 	return 0;
+}
+void Listado_de_Procesos(){
+	
 }
