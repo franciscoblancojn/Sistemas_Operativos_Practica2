62f653dd74c8a8f530a45a73ce4c29f1bf1955ff
ters <blancofrancisco34@gmail.com>
2018-05-30 18:12:20 -0400

    Agregando funciones al menu en metodos separados de main

--- Programa.c
+++ Programa.c
@@ -4,7 +4,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+void Ejecutar_Proceso();
 void Listado_de_Procesos();
+void Matar_Proceso();
 int main(int argc, char const *argv[])
 {
 	int opcion=-1;
@@ -16,11 +18,14 @@ int main(int argc, char const *argv[])
 		printf("3)Matar Proceso\n");
 		printf("4)Salir\n");
 		scanf("%i",&opcion);
+
+
 		if(opcion==1){
+			Ejecutar_Proceso();
 		}else if(opcion==2){
-
+			Listado_de_Procesos();
 		}else if(opcion==3){
-
+			Matar_Proceso();
 		}
 
 	}while(opcion!=4);
@@ -30,6 +35,11 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+void Ejecutar_Proceso(){
+
+}
 void Listado_de_Procesos(){
-	
+}
+void Matar_Proceso(){
+
 }
