a9a1a44585685cc6266790e02bf7990d72411b05
ters <blancofrancisco34@gmail.com>
2018-06-01 13:29:27 -0400

    Modificando Menu
    Se cambio la variable opcion de int a char para posibler errores del usuario
    Se agrego la funcion system("clear") para mejorar la presentacion del menu

--- Programa.c
+++ Programa.c
@@ -10,27 +10,28 @@ void Listado_de_Procesos();
 void Matar_Proceso();
 int main(int argc, char const *argv[])
 {
-	int opcion=-1;
+	char opcion='0';
 
 	do{
 		fflush(stdin);
+		system("clear");
 		printf("MENU\n");
 		printf("1)Ejecutar Proceso\n");
 		printf("2)Listado de Procesos\n");
 		printf("3)Matar Proceso\n");
 		printf("4)Salir\n");
-		scanf("%i",&opcion);
+		scanf("%c",&opcion);
 
 
-		if(opcion==1){
+		if(opcion=='1'){
 			Ejecutar_Proceso();
-		}else if(opcion==2){
+		}else if(opcion=='2'){
 			Listado_de_Procesos();
-		}else if(opcion==3){
+		}else if(opcion=='3'){
 			Matar_Proceso();
 		}
 
-	}while(opcion!=4);
+	}while(opcion!='4');
 
 
 
